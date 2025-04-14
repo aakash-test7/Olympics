e23be55827f2326b2c09cd9f7339ef8cff80e2f9
Aakash Kharb <akgdsc7@gmail.com>
2025-04-14 22:27:51 +0530

    TechWill x Olympics

---
+++ pages/__init__.py
@@ -0,0 +1,5 @@
+from pages.dataset_page import dataset_page
+from pages.information import info_page
+from pages.prediction import predict_page
+from pages.home import home_page
+from pages.chatbot import chat_page
